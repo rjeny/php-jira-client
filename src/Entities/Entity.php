93db1b6067abb4d3d90749a155c4e6f08ed6a0fe
rjeny <PipiN1702>
2017-09-22 02:01:01 +0300

    Add Entity Abstract

---
+++ src/Entities/Entity.php
@@ -0,0 +1,86 @@
+<?php
+namespace Rjeny\Jira\Entities;
+
+use \Rjeny\Jira\JiraClient;
+
+/**
+ * Class Entity
+ * @package Rjeny\Jira\Entities
+ */
+abstract class Entity
+{
+    /**
+     * Name. Used in URI
+     * @var string
+     */
+    protected $name;
+
+    /**
+     * Jira Client
+     * @var JiraClient
+     */
+    protected $client;
+
+    /**
+     * @var string
+     */
+    protected $key;
+
+    /**
+     * @var int
+     */
+    protected $id;
+
+    /**
+     * @var mixed
+     */
+    protected $data;
+
+    function __construct(JiraClient $client)
+    {
+        $this->client = $client;
+    }
+
+    /**
+     * Return url for use in browsers?
+     *
+     * @return mixed
+     */
+    abstract public function getUrl();
+
+    /**
+     * Preparing request data
+     *
+     * @return mixed
+     */
+    abstract public function prepareRequestData();
+
+    public function getID(){
+        if ($this->key){
+            return $this->key;
+        } elseif ($this->id) {
+            return $this->id;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Getter return class params or data info
+     *
+     * @param $name
+     * @return null
+     */
+    public function __get($name)
+    {
+        if (isset($this->$$name)) {
+            return $this->$$name;
+        }
+
+        if (isset($this->data[$name])){
+            return $this->data[$name];
+        }
+
+        return null;
+    }
+}
