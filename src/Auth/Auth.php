010c60dceb75694539133fafd7ee87197d54bcd3
rjeny <PipiN1702>
2017-09-22 01:59:59 +0300

    Add Auth classes (just Base now)

---
+++ src/Auth/Auth.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Rjeny\Jira\Auth;
+
+/**
+ * Interface for auth
+ */
+interface Auth
+{
+    public function setAuthHead(&$ch);
+}
---
+++ src/Auth/Base.php
@@ -0,0 +1,20 @@
+<?php
+namespace Rjeny\Jira\Auth;
+
+class Base implements Auth
+{
+    private $username;
+
+    private $password;
+
+    function __construct($username, $password)
+    {
+        $this->username = $username;
+        $this->password = $password;
+    }
+
+    public function setAuthHead(&$ch)
+    {
+        curl_setopt($ch, CURLOPT_USERPWD, $this->username . ':' . $this->password);
+    }
+}
