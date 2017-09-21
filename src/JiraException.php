90abdde94f56de60a1075049ea97c8aa32b9eb65
rjeny <PipiN1702>
2017-09-22 02:00:38 +0300

    Add Exception Class

---
+++ src/JiraException.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: SergeyPC
+ * Date: 21.09.2017
+ * Time: 21:46
+ */
+
+namespace Rjeny\Jira;
+
+/**
+ * Class JiraException
+ */
+class JiraException extends \Exception
+{
+}
